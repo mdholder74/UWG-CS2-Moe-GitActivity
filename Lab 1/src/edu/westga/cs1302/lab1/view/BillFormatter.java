ea7efdc384a1acfe2ad4637c340387ac9f3ccd6d
mdholder74 <mdholder74@gmail.com>
2025-08-23 08:58:32 -0400

    Separated view display logic from model logic
    1) Created View Package
    2) Created a new class inside the view package to handle the view
    display logic
    3) Added a getter method to the Bill class to get the list of items in a
    Bill

---
+++ Lab 1/src/edu/westga/cs1302/lab1/view/BillFormatter.java
@@ -0,0 +1,54 @@
+package edu.westga.cs1302.lab1.view;
+
+import edu.westga.cs1302.lab1.model.Bill;
+import edu.westga.cs1302.lab1.model.BillItem;
+
+/** Formats bill information for display.
+*
+* @author Moe Holder
+* @version Fall 2025
+*/
+public class BillFormatter {
+  
+
+  /** Create a new BillFormatter.
+  *
+  * @precondition none
+  * @postcondition none
+  */
+  public BillFormatter() {
+
+  }
+
+  /** Return a String containing the list of bill items and total for the bill.
+  *
+  * @precondition customerBill != null
+  * @postcondition none
+  * @param customerBill the bill to format
+  * @return a String containing the list of bill items and total for the bill
+  */
+
+  public String getText(Bill customerBill) {
+    if (customerBill == null) {
+      throw new IllegalArgumentException("Invalid: customer bill cannot be null.");
+    }
+
+    String text = "ITEMS" + System.lineSeparator();
+    double subTotal = 0.0;
+    for (BillItem item : customerBill.getItems()) {
+      text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+      subTotal += item.getAmount();
+    }
+
+    text += System.lineSeparator();
+    text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
+    double tax = subTotal * 0.1;
+    double tip = subTotal * 0.2;
+    text += "TAX - $" + tax + System.lineSeparator();
+    text += "TIP - $" + tip + System.lineSeparator();
+    text += "TOTAL - $" + (subTotal + tip + tax);
+
+    return text;
+  }
+
+}
