e326f90677509e3f3988873e89b16cd5ecb98d25
mdholder74 <mdholder74@gmail.com>
2025-09-01 14:37:46 -0400

    Added unit tests for the BillItem Constructor
    - Created TestBillItemConstructor class with 3 test methods covering:
    1) Valid name and amount return
    2) Null input validation for name with exception handling
    3) Null input validation for amount with exception handling
    
    - Followed AAA (Arrange-Act-Assert) testing pattern
    - Used assertThrows(), Used assertEqual() and Used assertNotNull() in testing

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestBillItemConstructor.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestBillItemConstructor.java
@@ -4,11 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.lab2.model.BillItem;
+
 class TestBillItemConstructor {
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  public void testConstructor_WithValidNameAndAmount() {
+    
+    BillItem item = new BillItem("Gum", 1.99);
+    
+    assertNotNull(item, "Constructor should create a non-null object.");
+    assertEquals("Gum", item.getName(), "Name should match the provided value.");
+    assertEquals(1.99, item.getAmount(), 0.001, "Amount should match the provided value.");
+    
   }
+  
+  @Test
+  public void testConstructor_WithNullName() {
+   IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+       new BillItem(null, 1.99);
+      });
+   
+   assertEquals("name must not be null.", exception.getMessage(),
+       "Exception message should match name must not be null.");
+   }
+  
+  @Test
+  public void testConstructor_WithNullAmount() {
+   IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+       new BillItem("Gum", -1.99);
+      });
+   
+   assertEquals("amount must be positive, but was -1.99", exception.getMessage(),
+       "Exception message should match amount must be positive.");
+   }
 
 }
