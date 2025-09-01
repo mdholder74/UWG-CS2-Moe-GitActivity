1d7e556afbbb7f33d79cca1d92493e20206ab45d
mdholder74 <mdholder74@gmail.com>
2025-09-01 13:43:48 -0400

    Added unit tests for Bill.getItems() method
    - Created TestGetItems class with 4 test methods covering:
    1) Valid single item returned
    2) Valid mulitple items return
    3) Valid return for empty
    4) Valid reutn for Null
    
    - Implemented @BeforeEach setup method for consistent test initialization
    - Used assertEquals(), Used assertTrue() and Used assertNotNull() in testing
    - These test ensure the getItems() returns the correct bill state

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestGetItems.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill/TestGetItems.java
@@ -2,13 +2,50 @@ package edu.westga.cs1302.lab2.tests.model.bill;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+
 class TestGetItems {
+  
+  private Bill moeBill;
+  
+  @BeforeEach
+  public void setUp() {
+    this.moeBill = new Bill();
+  }
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  public void testGetItems_NotNull() {
+    assertNotNull(this.moeBill.getItems());
+    
+  }
+  
+  @Test
+  public void testGetItems_InitiallyEmpty() {
+    assertTrue(this.moeBill.getItems().isEmpty());
+  }
+  
+  @Test
+  public void testGetItems_AddingOneItem() {
+    BillItem item = new BillItem("Peach Rings", 2.99);
+    this.moeBill.addItem(item);
+    assertEquals(1, this.moeBill.getItems().size());
+  }
+  
+  @Test
+  public void testGetItems_AddingMultipleItems() {
+    BillItem item1 = new BillItem("Peach Rings", 2.99);
+    BillItem item2 = new BillItem("Gummy Bears", 3.49);
+    
+    this.moeBill.addItem(item1);
+    this.moeBill.addItem(item2);
+    
+    assertEquals(2, this.moeBill.getItems().size());
+    assertEquals(item1, this.moeBill.getItems().get(0));
+    assertEquals(item2, this.moeBill.getItems().get(1));
   }
 
 }
