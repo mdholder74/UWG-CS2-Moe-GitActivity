723cd108dda920037ed0e6fd81ef18b50a538b66
mdholder74 <mdholder74@gmail.com>
2025-09-08 12:22:01 -0400

    Implement GUI behavior for the Bill system
    1) Declared private fields for Bill and BillView instances for state persistence
    2) Implemented getText() to retrieve user input name and amount from JavaFx TextFields. Implemented type conversion for convert the amount
    3) Created BillItem object from user input (name and amount) to be add to the Bill object
    4) Integrated BillView and used getText to get the bill information and format it
    5) Implemented setText() to display the Bill's information in the textarea
    
    Architecture: Uses the MVC pattern model

--- code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,5 +1,12 @@
 package edu.westga.cs1302.lab3.views;
 
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,11 +15,45 @@ package edu.westga.cs1302.lab3.views;
  * @version Fall 2025
  */
 public class MainWindow {
-    
+  private Bill bill;
+  private BillView billView;
+  
+
+    @FXML
+    private TextField amount;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextArea output;
+
+    @FXML
+    void addItem(ActionEvent event) {
+      String billName = this.name.getText();// get the name from the text field
+      double billAmount = Double.parseDouble(this.amount.getText());// get the amount from the text field
+      
+      BillItem billItem = new BillItem(billName, billAmount);// create a new bill item object using the name and amount
+      
+      bill.addItem(billItem);// add the bill item (name and amount) to the bill object
+      
+      String billInfo = this.billView.getText(bill);// get the bill information, format it and display it in the text area
+      this.output.setText(billInfo);// display the bill information in the text area
+      
+
+    }
+
+
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
     public void initialize() {
+      this.bill = new Bill();// create a new bill object
+      this.billView = new BillView();// create a new bill view object
     	
     }
+    
+    
+    
+    
 }
