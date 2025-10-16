7046df84c1b2735592c6b886cb123d21a99eaf31
mdholder74 <mdholder74@gmail.com>
2025-10-16 08:35:32 -0400

    Test cases inside for Ascedning and Descending classes
    1) Test Ascending comparator orders tasks LOW to HIGH
    2) Test Descending comparator orders tasks HIGH to LOW
    
    This ensures the correct sortign behavior with the UI ListView integration.

--- code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/ascending/TestCompare.java
+++ code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/ascending/TestCompare.java
@@ -4,11 +4,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.task_tracker.model.Ascending;
+import edu.westga.cs1302.task_tracker.model.Task;
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
 class TestCompare {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testTheLowestPriorityComesFirst() {
+    Ascending comparator = new Ascending();
+    Task lowPriorityTask = new Task("Low Priority Task", "descripton", TaskPriority.LOW);
+    Task highPriorityTask = new Task("High Priority Task", "descripton", TaskPriority.HIGH);
+    assertTrue(comparator.compare(lowPriorityTask, highPriorityTask) < 0);
+  }
+	
+	@Test
+	void testTheLowestPriorityComesbeforeMedium() {
+    Ascending comparator = new Ascending();
+    Task lowPriorityTask = new Task("Low Priority Task", "descripton", TaskPriority.LOW);
+    Task mediumPriorityTask = new Task("Medium Priority Task", "descripton", TaskPriority.MEDIUM);
+    assertTrue(comparator.compare(lowPriorityTask, mediumPriorityTask) < 0);
+  }
+	
+	@Test
+	void testTheMediumPriorityComesBeforeHigh() {
+    Ascending comparator = new Ascending();
+    Task mediumPriorityTask = new Task("Medium Priority Task", "descripton", TaskPriority.MEDIUM);
+    Task highPriorityTask = new Task("High Priority Task", "descripton", TaskPriority.HIGH);
+    assertTrue(comparator.compare(mediumPriorityTask, highPriorityTask) < 0);
+  }
+	
+	@Test
+	void testSamePriorityLowIsEqual() {
+    Ascending comparator = new Ascending();
+    Task lowPriorityTask1 = new Task("Low Priority Task 1", "descripton", TaskPriority.LOW);
+    Task lowPriorityTask2 = new Task("Low Priority Task 2", "descripton", TaskPriority.LOW);
+    assertEquals(0, comparator.compare(lowPriorityTask1, lowPriorityTask2));
+  }
+	
+	@Test
+  void testSamePriorityMediumIsEqual() {
+	  Ascending comparator = new Ascending();
+    Task mediumPriorityTask1 = new Task("Medium Priority Task 1", "descripton", TaskPriority.MEDIUM);
+    Task mediumPriorityTask2 = new Task("Medium Priority Task 2", "descripton", TaskPriority.MEDIUM);
+    assertEquals(0, comparator.compare(mediumPriorityTask1, mediumPriorityTask2));
+  }
+	
+	@Test
+	  void testSamePriorityHighIsEqual() {
+    Ascending comparator = new Ascending();
+    Task highPriorityTask1 = new Task("High Priority Task 1", "descripton", TaskPriority.HIGH);
+    Task highPriorityTask2 = new Task("High Priority Task 2", "descripton", TaskPriority.HIGH);
+    assertEquals(0, comparator.compare(highPriorityTask1, highPriorityTask2));
+  }
 
 }
